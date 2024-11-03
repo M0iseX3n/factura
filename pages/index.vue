c478a3b647fceaaa71f96e27ac50fea43ae72932
LuisjavierGonzalez <163852372+LuisjavierGonzalez@users.noreply.github.com>
2024-11-03 17:53:22 -0500

    luis
    precios y descripciónes

--- pages/index.vue
+++ pages/index.vue
@@ -26,43 +26,43 @@ export default {
           productos : [
             {
                 imagen : 'https://img.freepik.com/foto-gratis/zapatos-cuero-marron_1203-8175.jpg',
-                precio :  290,
+                precio :  220,
                 titulo : 'Zapato clasico ',
-                descripcion : 'fgf'
+                descripcion : 'Es el complemento ideal para un estilo sofisticado y elegante.'
             },
              {
                 imagen : 'https://http2.mlstatic.com/D_696111-MLA70741601961_072023-C.jpg',
-                precio : 3000,
+                precio : 150,
                 titulo : 'zapato juvenil',
-                descripcion : 'HGHGFHJF',
+                descripcion : 'Su diseño versátil y cómodo es perfecto para cualquier ocasión.',
 
              } ,
              {
                 imagen : 'https://azzurry.com/wp-content/uploads/2023/10/0251_BQ6806-122.jpg',
-                precio : 3000,
+                precio : 125,
                 titulo : 'Nike Blazer',
-                descripcion : 'HGHGFHJF',
+                descripcion : 'Los Nike Blazer son un ícono de la moda urbana y deportiva.',
 
              } ,
              {
                 imagen : 'https://d2cva83hdk3bwc.cloudfront.net/onitsuka-tiger-mexico-66-yellow-black-1.jpg',
-                precio : 3000,
+                precio : 184,
                 titulo : 'Onitsika tiger mexico 66',
-                descripcion : 'HGHGFHJF',
+                descripcion : 'Son la combinación perfecta de estilo retro y comodidad moderna.',
 
              } ,
              {
                 imagen : 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3bbecbdf584e40398446a8bf0117cf62_9366/Tenis_Samba_OG_Blanco_B75806_01_standard.jpg',
-                precio : 3000,
+                precio : 200,
                 titulo : 'Adidas Samba',
-                descripcion : 'HGHGFHJF',
+                descripcion : 'Un verdadero clásico de Adidas una mezcla perfecta de estilo y funcionalidad.',
 
              } ,
              {
                 imagen : 'https://pufferreds.com/cdn/shop/files/A07893C-1.png?v=1714629148',
-                precio : 3000,
+                precio : 150,
                 titulo : 'Converse one star',
-                descripcion : 'HGHGFHJF',
+                descripcion : 'Aportan un toque clásico y atemporal a cualquier look.',
 
              } ,
 
